023c9f0c42fa547a0e93a47a8a9a68dd4c8d9fcf
bangarangler <bangarangler@gmail.com>
2021-01-12 18:18:17 -0500

    might have figured out way around pulling axios into context...
    testing further tomorrow.

--- client/src/pages/private-area.tsx
+++ client/src/pages/private-area.tsx
@@ -3,80 +3,25 @@ import Cookies from "js-cookie";
 import Me from "../components/Me/me";
 import { useRouter } from "next/router";
 import { isServer } from "../utils/isServer";
-import { useMeQuery } from "../generated/graphql";
-import { GQL_ENDPOINT } from "../../constants";
-// import { useQuery } from "react-query";
-// import gql from "graphql-tag";
-// import { useUserContext } from "../context/allContexts";
+// import { useMeQuery } from "../generated/graphql";
+// import { GQL_ENDPOINT } from "../../constants";
 import { useAxiosContext } from "../context/allContexts";
-import { GraphQLClient } from "graphql-request";
+// import { GraphQLClient } from "graphql-request";
 import useLogout from "../react-query-hooks/useLogout";
-// import useMeData from "../react-query-hooks/useMe";
 // import Todos from "../components/Todos/Todos.tsx";
+// import useLogout from '../react-query-hooks/useLogout'
 
 const PrivateArea = () => {
   // next-router
   const router = useRouter();
-  // const { axios } = useAxiosContext();
   // User Context
-  // const { userEmail, token } = useUserContext();
   const { user, token } = useAxiosContext();
 
-  // const { data: meData, status } = useMeData({ email: user?.email });
-  // Must be a way to extract this with react-query if not turn into custom hook
-  // const graphQLClient = new GraphQLClient(GQL_ENDPOINT, {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //     Authorization: token ? `Bearer ${token}` : "",
-  //   },
-  //   credentials: "include",
-  // });
-  // const { data: meData, status } = useMeQuery(
-  //   graphQLClient,
-  //   {
-  //     email: user?.email,
-  //   },
-  //   // will not run until it has token and userEmail (both of which required)
-  //   { enabled: !!token && !!user.email }
-  // );
-  // console.log("status", status);
-  // console.log("data from useMe", meData);
-
-  // useEffect(() => {
-  //   console.log("user from private", user);
-  //   console.log("meData from private", meData);
-  //   console.log("status from private", status);
-  // }, [user, token, status, meData]);
-  // renamed so not to cause a conflict
   const {
     mutate: logoutMutate,
     data: logoutData,
     status: logoutStatus,
   } = useLogout();
-
-  // useEffect to deal with meData
-  // useEffect(() => {
-  //   switch (status) {
-  //     case "loading":
-  //       console.log("meData loading...");
-  //       break;
-  //     case "idle":
-  //       console.log("meData isIdle");
-  //       break;
-  //     case "error":
-  //       console.log("meData error");
-  //       break;
-  //     case "success":
-  //       console.log("meData", meData);
-  //       if (meData.status === 404) {
-  //         console.log("nope that didn't work");
-  //         return;
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }, [status, meData]);
 
   // logout function to run using custom react-query hook
   const logout = async () => {
--- client/src/react-query-hooks/useLogout.ts
+++ client/src/react-query-hooks/useLogout.ts
@@ -1,22 +1,19 @@
 import { useMutation } from "react-query";
 import { REST_BASE_ENDPOINT } from "../../constants";
+import { axios } from "../utils/axiosConfig";
 
 const postLogout = async () => {
-  const data = await fetch(`${REST_BASE_ENDPOINT}/auth/logout`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-    // body: JSON.stringify(registerInput),
-  });
+  const data = await axios.post(`${REST_BASE_ENDPOINT}/auth/logout`);
 
   // console.log("data from useLogout", data);
-  const json = await data.json();
+  // const json = await data.json();
   // console.log("json from useLogout", json);
   // console.log("json.data from useLogout", json.data);
 
-  return json.data;
+  // return json.data;
+  if (data) {
+    return data.data.data;
+  }
 };
 
 export default function useLogout() {
