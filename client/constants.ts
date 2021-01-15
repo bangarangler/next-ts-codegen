f563aa5f289a3836a367418ffe2b1e62e1cb75bd
bangarangler <bangarangler@gmail.com>
2021-01-15 15:32:03 -0500

    fixed typo

--- client/constants.ts
+++ client/constants.ts
@@ -8,6 +8,7 @@ export const GQL_SUBSCRIPTION_ENDPOINT =
     ? "ws://nodereacttesting.nowigence.ai/graphql"
     : "ws://localhost:4000/graphql";
 
-export const REST_BASE_ENDPOINT = (process.env.NODE_ENV = "production"
-  ? "https://nodereacttesting.nowigence.ai"
-  : "http://localhost:4000");
+export const REST_BASE_ENDPOINT =
+  process.env.NODE_ENV === "production"
+    ? "https://nodereacttesting.nowigence.ai"
+    : "http://localhost:4000";
