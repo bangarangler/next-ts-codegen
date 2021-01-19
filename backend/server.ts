d739e852c874bd0344f3e37fc23ce3e65f2954ed
bangarangler <bangarangler@gmail.com>
2021-01-19 12:59:52 -0500

    added redis-aiven service

--- backend/server.ts
+++ backend/server.ts
@@ -32,6 +32,8 @@ try {
   const redisOptions = {
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: process.env.REDIS_PORT || "6379",
+    password: process.env.REDIS_PASSWORD,
+    tls: true,
     retryStrategy: (times: any) => {
       return Math.min(times * 50, 2000);
     },
