b93fa1fbe17862cb5184489ebfca613e2ca9f285
bangarangler <bangarangler@gmail.com>
2021-01-22 19:17:00 -0500

    testing

--- backend/serverProd.ts
+++ backend/serverProd.ts
@@ -71,10 +71,12 @@ try {
   // app.use(express.static(path.join(__dirname, "/build/static/js")));
   app.use(express.static(path.join(__dirname, "/build")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     console.log("dirname", __dirname);
-    console.log("dirname", __dirname + "/build/index.html");
-    res.sendFile(path.join(__dirname + "/build/index.html"));
+    // console.log("dirname", __dirname + "/build/index.html");
+    console.log("dirname", __dirname, "build", "index.html");
+    // res.sendFile(path.join(__dirname + "/build/index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 
   app.use(authMiddleware);
