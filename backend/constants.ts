5c3652a852af71adfe9c373f0e07e466c7f4e4e5
bangarangler <bangarangler@gmail.com>
2021-01-25 14:53:13 -0500

    Added *

--- backend/constants.ts
+++ backend/constants.ts
@@ -12,8 +12,9 @@ export const __prod_cors__ =
       }
     : {
         origin: [
-          "https://client-cookies-test.vercel.app/",
-          "https://bang-k8s.com",
+          // "https://client-cookies-test.vercel.app/",
+          // "ws://client-cookies-test.vercel.app/",
+          "https://bang-k8s.com/*",
           "https://bang-k8s.com/graphql",
           "ws://bang-k8s.com/graphql",
         ],
