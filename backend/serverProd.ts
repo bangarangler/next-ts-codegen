f7cd744ddb2c4fb64ef73660deffd4431de61c42
bangarangler <bangarangler@gmail.com>
2021-01-26 16:06:29 -0500

    testing ssh with loadbalancer

--- backend/serverProd.ts
+++ backend/serverProd.ts
@@ -63,26 +63,26 @@ try {
   });
   app.use(authMiddleware);
 
-  const privateKey = fs.readFileSync(
-    `/etc/letsencrypt/live/bang-k8s.com/privkey.pem`,
-    "utf8"
-  );
-
-  const certificate = fs.readFileSync(
-    `/etc/letsencrypt/live/bang-k8s.com/cert.pem`,
-    "utf8"
-  );
-
-  const ca = fs.readFileSync(
-    `/etc/letsencrypt/live/bang-k8s.com/chain.pem`,
-    "utf8"
-  );
-
-  const credentials: any = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca,
-  };
+  // const privateKey = fs.readFileSync(
+  //   `/etc/letsencrypt/live/bang-k8s.com/privkey.pem`,
+  //   "utf8"
+  // );
+  //
+  // const certificate = fs.readFileSync(
+  //   `/etc/letsencrypt/live/bang-k8s.com/cert.pem`,
+  //   "utf8"
+  // );
+  //
+  // const ca = fs.readFileSync(
+  //   `/etc/letsencrypt/live/bang-k8s.com/chain.pem`,
+  //   "utf8"
+  // );
+  //
+  // const credentials: any = {
+  //   key: privateKey,
+  //   cert: certificate,
+  //   ca: ca,
+  // };
 
   const context = async ({ req, res, connection, redis }: ServerContext) => {
     if (connection) {
@@ -113,7 +113,8 @@ try {
     // }
   };
   const httpServer = http.createServer(app);
-  const httpsServer = https.createServer(credentials, app);
+  const httpsServer = https.createServer(app);
+  // const httpsServer = https.createServer(credentials, app);
 
   const server = new ApolloServer({
     typeDefs,
